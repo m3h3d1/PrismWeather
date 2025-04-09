9a8f495bdef4c1162f8e3ca04ce966c2186f40e9
Mehedi Hasan (Parvez) <39239695+m3h3d1@users.noreply.github.com>
2025-04-09 11:25:53 +0600

    Initial commit

---
+++ src/main/java/com/mehedi/prismweather/PrismWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.mehedi.prismweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrismWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrismWeatherApplication.class, args);
+	}
+
+}
